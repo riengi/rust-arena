b00135f81ec9a1f7bc0246121861e07312e90eb5
riengi <112477297+riengi@users.noreply.github.com>
2022-09-02 11:41:35 +0200

    Basic vecor and pseudo-mixed enum vector code added

---
+++ collections/src/main.rs
@@ -0,0 +1,56 @@
+// Collections
+fn main() {
+    basic_vector();
+    various_types_vector();
+}
+
+// Basic vector with integers
+fn basic_vector() {
+    let v = vec![1, 2, 3];
+
+    print!("Basic vector: ");
+    for i in v {
+        print!("{},", i);
+    }
+    println!();
+}
+
+// Vector containing various types stored in enum
+fn various_types_vector() {
+    #[derive(Debug)]
+    struct Point {
+        x: u32,
+        y: u32,
+    }
+    impl Point {
+        fn new(x: u32, y: u32) -> Self {
+            Self { x, y }
+        }
+    }
+
+    #[derive(Debug)]
+    enum Type {
+        Int(i32),
+        UInt(u32),
+        Str(String),
+        Point(Point),
+    }
+
+    print!("Pseudo-mixed vector: ");
+
+    let i = Type::Int(-32);
+    let u = Type::UInt(32);
+    let s = Type::Str(String::from("I'm String"));
+    let p = Type::Point(Point::new(1, 1));
+
+    let v = vec![i, u, s, p];
+
+    for x in v {
+        if let Type::Point(value) = x {
+            print!("Point({},{})", value.x, value.y);
+        } else {
+            print!("{:?},", x);
+        }
+    }
+    println!();
+}
